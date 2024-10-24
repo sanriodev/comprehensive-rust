d4e3fa4cf596ef93666fb9b9f39457b81c52ba26
Matteo Juen <matteo.juen@ematric.com>
2024-10-24 10:30:07 +0200

    transpose a matrix

--- two/src/array_handling.rs
+++ two/src/array_handling.rs
@@ -1,3 +1,5 @@
+use std::ptr::null;
+
 pub fn array_intro() {
     let arr: [i32; 2] = [2,15]; // array type declaration -> [type; size] -> [i32; 10]
     //length also matters for type, which means two int-arrays of different lengths are different types
@@ -13,4 +15,34 @@ pub fn tuples_instro() {
     let (x, y, z) = tup;
     println!("Tuple destructuring: x: {}, y: {}, z: {}", x, y, z);
     println!("Tuple indexing: x: {}, y: {}, z: {}", tup.0, tup.1, tup.2);
+}
+
+pub fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let start_matrix = matrix;
+    let mut finished: [[i32; 3]; 3] = [[0; 3]; 3];
+
+    for i in 0..start_matrix.len() {
+        for j in 0..start_matrix[i].len() {
+            finished[j][i] = start_matrix[i][j];
+        }
+    }
+    return finished;
+}
+
+#[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
 }
--- two/src/main.rs
+++ two/src/main.rs
@@ -1,4 +1,4 @@
-use array_handling::{array_intro, tuples_instro};
+use array_handling::{array_intro, transpose, tuples_instro};
 
 mod array_handling;
 
@@ -6,4 +6,14 @@ fn main() {
     array_intro();
 
     tuples_instro();
+
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix: {:#?}", matrix);
+    let transposed = transpose(matrix);
+    println!("transposed: {:#?}", transposed);
 }
